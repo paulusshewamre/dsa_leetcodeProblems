d9b01f8d29bde3e3005f48fb9bc588a89652aea3
Paulus <113542966+paulusshewamre@users.noreply.github.com>
2026-02-08 17:46:32 +0300

    Time: 11 ms (54.51%), Space: 19.7 MB (55.13%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -1,20 +1,15 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        common = set(list1) & set(list2)
-        counter = {}
-        for i in range(len(list1)):
-            if list1[i] in common:
-                counter[list1[i]] = counter.get(list1[i], 0) + i
-        
-        for i in range(len(list2)):
-            if list2[i] in common:
-                counter[list2[i]] = counter.get(list2[i], 0) + i
-        
+        counter = { word: i for i, word in enumerate(list1)}
+        minSum = float('inf')
         res = []
-        sorted_items = sorted(counter.items(), key=lambda item:item[1])
-        minn = sorted_items[0][1]
-        for key, value in sorted_items:
-            if value <= minn:
-                res.append(key)
-        
+
+        for j , word in enumerate(list2):
+            if word in counter:
+                indexSum = j + counter[word]
+                if indexSum < minSum:
+                    minSum = indexSum
+                    res = [word]
+                elif indexSum == minSum:
+                    res.append(word)
         return res
