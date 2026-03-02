b0848a204c8221bd6479ed3658c419ed75bb37f6
Paulus <113542966+paulusshewamre@users.noreply.github.com>
2026-03-02 21:01:36 +0300

    Time: 3 ms (97.45%), Space: 20.2 MB (84.62%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,19 +1,16 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        mp = {}
-        for i in range(len(s)):
-            mp[s[i]] = mp.get(s[i], 0) + 1
+        mp = Counter(s)
         
-        arr = [0] * len(s)
-        sortedmpArray= sorted(mp.items(), key=lambda item:item[1], reverse=True)
+        res = []
+        sortedmp = mp.most_common()
+
 
         i = 0
-        for key, val in sortedmpArray:
-            while val > 0:
-                arr[i] = key
-                i+=1
-                val-=1
+        for char, count  in sortedmp:
+            res.append(char*count)
+
         
-        return "".join(arr)
+        return "".join(res)
             
 
