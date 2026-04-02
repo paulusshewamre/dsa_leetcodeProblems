0eac5ed17f667ec7ea2e24361d55ed3e8eb03120
Paulus <113542966+paulusshewamre@users.noreply.github.com>
2026-04-02 22:23:03 +0300

    Time: 3 ms (45.16%), Space: 22.3 MB (79.42%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -8,12 +8,12 @@ class Solution:
         dummy = ListNode(0, head)
         prev = dummy
         cur = head
-        while cur != None:
+
+        while cur:
             if cur.val == val:
                 prev.next = cur.next
             else:
                 prev = cur
-
-            cur = cur.next  
+            cur = cur.next
         
         return dummy.next
