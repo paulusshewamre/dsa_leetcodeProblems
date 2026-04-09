eed44910e234a76e64a258925c92cd327dc9029a
Paulus <113542966+paulusshewamre@users.noreply.github.com>
2026-04-09 16:13:39 +0300

    Time: 0 ms (100%), Space: 19.5 MB (15.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,15 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = [-1] * len(nums1)
-        for i, n in enumerate(nums1):
-            flag = False
-            for m in nums2:
-                if n == m:
-                    flag = True
-                if flag and m > n:
-                    res[i] = m
-                    break
-        return res
+        mp = {}
+        stk = []
+
+        for num in reversed(nums2):
+            while stk and stk[-1] <= num:
+                stk.pop()
             
+            mp[num] = -1 if not stk else stk[-1]
+            stk.append(num)
+        
+        return [mp[num] for num in nums1]
                     
 
